61a9d81b33e54b8c26fcf640c2536124c2b351fc
Stefania Sharp <sharstef@amazon.com>
2022-04-14 15:13:23 -0700

    Update for Python 3

--- model_exploder.py
+++ model_exploder.py
@@ -1,9 +1,9 @@
 """
  Language Model Exploder
  Author: Stefania Sharp
- Last Updated: 3/21/2022
+ Last Updated: 4/14/2022
 
- To Run: python model_exploder.py myModel.json
+ To Run: python exploder.py myModel.json
  Output: csv file with two columns (utterance and intent)
  
  Takes in a language model in any language and outputs a file with 
@@ -27,19 +27,39 @@ def importJSON(input):
     try:                                                                           
         data = json.load(input)
 
-        if data.has_key("interactionModel") and data["interactionModel"].has_key("languageModel"):
-            return data["interactionModel"]["languageModel"]
+        if "interactionModel" in data and "languageModel" in data["interactionModel"]:
+            return data["interactionModel"]
         else:                                               
             print("Input file is not a supported Alexa language model. Must contain attributes interactionModel and languageModel.")     
-            return None                                                  
+            return None    
+
     except ValueError:                        
         print("Invalid format. Input file must be JSON.")                                                        
         return None 
 
 def formatLine(line):
-    line = re.sub(ur"[^\w\d'.\s]+",'', line) 
+    line = re.sub(r"[^\w\d'.\s]+",'', line) 
     line = line.lower()
+    line = line.rstrip()
     return line
+
+def compressSlots(data):
+    # Compress the slots down into arrays
+    if "types" in data and len(data["types"]) > 0:
+        slots = {}
+
+        for type in data["types"]:
+            slotName = type["name"]
+            slots[slotName] = []
+            for value in type["values"]:
+                # Add the synonyms and the value
+                if "value" in value["name"]:
+                    slots[slotName].append(value["name"]["value"])
+
+                if "synonyms" in value["name"]:
+                    slots[slotName] = slots[slotName] + value["name"]["synonyms"]
+
+        return slots
 
 def main():
     # Get name of json file from args
@@ -63,28 +83,15 @@ def main():
         os.remove(outFileName)
     outputFile = open(outFileName,"a")
 
-    # Compress the slots down into arrays
-    if data.has_key("types") and len(data["types"]) > 0:
-        slots = {}
-
-        for type in data["types"]:
-            slotName = type["name"]
-            slots[slotName] = []
-            for value in type["values"]:
-                # Add the synonyms and the value
-                if value["name"].has_key("value"):
-                    slots[slotName].append(value["name"]["value"])
-
-                if value["name"].has_key("synonyms"):
-                    slots[slotName] = slots[slotName] + value["name"]["synonyms"]
+    data = data["languageModel"]
+    slots = compressSlots(data)
 
     # Go through each intent and expand the utterances. Skip intents with Amazon built-in slots.
     for intent in data["intents"]:
         if len(intent["samples"]) > 0 and "AMAZON." not in intent["name"]:
 
-            # Print the intent to file to separate the utterance groups by intent
+            # Prep data holders
             name = intent["name"]
-            print("\nProcessing utterances for intent: "+name+"\n")
 
             # Find all Amazon slots for intent to skip utterances that contain them
             amazonSlots = []
@@ -101,8 +108,6 @@ def main():
 
                 # Skip any utterance that has an Amazon built-in
                 if not any(x in utterance for x in amazonSlots):
-
-                    #print("Processing utterance: "+utterance)
 
                     # Prep slots for itertools for this utterance
                     localSlots = []
